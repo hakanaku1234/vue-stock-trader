5d5dd0f72777b9d050a95239090888e77ec67a51
Jan Czizikow <jan.czizikow@gmail.com>
2018-06-07 19:51:25 +0200

    Trasition of main components when routing

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
     <div class="container">
       <router-view name="header"></router-view>
-      <router-view></router-view>
+      <transition name="fade" mode="out-in" appear>
+        <router-view></router-view>
+      </transition>
     </div>
 </template>
-
-<script>
-
-    export default {
-
-    }
-</script>
 
 <style>
 body {
   padding-top: 20px;
   padding-bottom: 20px;
+}
+
+.fade-enter {
+  opacity: 0;
+  transform: translateY(20px);
+}
+
+.fade-enter-active {
+  transition: all .3s ease;
+}
+
+.fade-leave-active {
+  opacity: 0;
+  transform: translateY(20px);
+  transition: all .3s ease;
 }
 </style>
